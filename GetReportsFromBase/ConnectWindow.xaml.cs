087681bc2ba1c8c67b36c9a31940b5840f521693
Tuzantar <113635527+Tuzantar@users.noreply.github.com>
2022-09-23 14:49:48 +0200

    2022-09-23

---
+++ GetReportsFromBase/ConnectWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace GetReportsFromBase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnect _sqlConnect;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((cmbx.Items.Count == 0) || cmbx.SelectedValue is null) 
+            { 
+                Connect(); 
+            }
+            else 
+            { 
+                Connect(cmbx.SelectedValue.ToString()); 
+            }
+        }
+        private void Connect()
+        {
+            _sqlConnect = new SqlConnect(txt_ip.Text, txt_id.Text, txt_pwd.Password);
+            _sqlConnect.Connect();
+            List<string> list = new List<string>(_sqlConnect.BaseList);
+            cmbx.Items.Clear();
+            foreach (string s in list)
+            {
+                cmbx.Items.Add(s);
+            }
+        }
+        private void Connect(string DbName)
+        {
+            _sqlConnect = new SqlConnect(txt_ip.Text, txt_id.Text, txt_pwd.Password, DbName);
+            _sqlConnect.Connect();
+        }
+    }
+}
---
+++ GetReportsFromBase/SqlConnect.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GetReportsFromBase
+{
+    internal class SqlConnect
+    {
+        private string _ServerName;
+        private string _DbName;
+        private string _UserId;
+        private string _Password;
+        private List<string> _BaseList;
+        private SqlConnection _conn = new SqlConnection();
+
+        public string ServerName { get { return _ServerName; } set { _ServerName = value; } }
+        public string DbName { get { return _DbName; } set { _DbName = value; } }
+        public string UserId { get { return _UserId; } set { _UserId = value; } }
+        public string Password { get { return _Password; } set { _Password = value; } }
+        public List<string> BaseList { get { return _BaseList; } set { _BaseList = value; } }
+
+        public SqlConnect(string ServerName, string UserId, string Password, string DbName)
+        {
+            _ServerName = ServerName;
+            _UserId = UserId;
+            _Password = Password;
+            _DbName = DbName;
+            Connect();
+        }
+        public SqlConnect(string ServerName, string UserId, string Password)
+        {
+            _ServerName = ServerName;
+            _UserId = UserId;
+            _Password = Password;
+            _DbName = "master";
+            Connect();
+        }
+        public void Connect()
+        {
+            _conn = new SqlConnection();
+            _conn.ConnectionString =
+            "Data Source=" + _ServerName.ToString() + ";" +
+            "Initial Catalog=" + _DbName.ToString() + ";" +
+            "User id=" + _UserId.ToString() + ";" +
+            "Password=" + _Password.ToString() + ";" +
+            "Integrated Security=false;";
+            if(_DbName == "master") { GetBaseList(); }
+        }
+
+        public void GetBaseList()
+        {
+            _conn.Open();
+            SqlCommand sql_cmnd = new SqlCommand("SELECT [name] FROM master.dbo.sysdatabases WHERE dbid > 4", _conn);
+            sql_cmnd.CommandType = CommandType.Text;
+            sql_cmnd.ExecuteNonQuery();
+            string sqlQuery = "SELECT [name] FROM master.dbo.sysdatabases WHERE dbid > 4";
+            SqlDataAdapter adpt = new SqlDataAdapter(sqlQuery, _conn);
+            DataTable table = new DataTable();
+            adpt.Fill(table);
+            _BaseList = new List<string>();
+            foreach(DataRow dr in table.Rows)
+            {
+                _BaseList.Add(dr.ItemArray[0].ToString());
+            }
+            _conn.Close();
+        }
+    }
+}
